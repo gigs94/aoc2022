6af93408ac638f00f0b90fa6ed1efd4c03bb579f
daniel gregory <daniel.a.gregory@raytheon.com>
2022-12-01 09:32:21 -0500

    day1

---
+++ day1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from collections import Counter
+
+with open('day1.dat') as data:
+    c={ int(i)+1:sum([int(x) for x in calories.strip().split('\n')]) for i,calories in enumerate(data.read().split('\n\n')) } 
+    new_val = Counter(c).most_common(3)
+    print(f'{new_val}, {sum([ x[1] for x in new_val])}')
