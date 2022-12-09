f5055f7ee7cef76cf9b9ef7216aff3c07fcb0b9c
daniel gregory <daniel.a.gregory@raytheon.com>
2022-12-08 22:30:30 -0500

    updated day1 to print both answers

--- day1.py
+++ day1.py
@@ -4,5 +4,7 @@ from collections import Counter
 
 with open('day1.dat') as data:
     c={ int(i)+1:sum([int(x) for x in calories.strip().split('\n')]) for i,calories in enumerate(data.read().split('\n\n')) } 
+    new_val = Counter(c).most_common(1)
+    print(f'{new_val}, {sum([ x[1] for x in new_val])}')
     new_val = Counter(c).most_common(3)
     print(f'{new_val}, {sum([ x[1] for x in new_val])}')
