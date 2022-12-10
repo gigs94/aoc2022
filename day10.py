978f95c71235950ed9ee119e0841309633b5806a
daniel gregory <daniel.a.gregory@raytheon.com>
2022-12-10 18:13:28 -0500

    day9 and 10

---
+++ day10.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python
+
+from aocd import lines
+
+test = """addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop"""
+
+test2 = """noop
+addx 3
+addx -5"""
+
+
+signals = {}
+crt = []
+
+
+def engine(c, x, s, a):
+    # DURING CYCLE
+    signals[c] = c * x
+    crt.append("#" if abs((c % 40) - s) <= 1 else ".")
+    c += 1
+    # AFTER CYCLE
+    if a:
+        x += int(a)
+    s = x
+    return c, x, s
+
+
+def process(lines):
+    l = [l.split() for l in lines]
+    c = 0
+    x = 1
+    s = x
+    for m in l:
+        if m[0] == "noop":
+            c, x, s = engine(c, x, s, None)
+        elif m[0] == "addx":
+            c, x, s = engine(c, x, s, None)
+            c, x, s = engine(c, x, s, m[1])
+
+    # c, x, s = engine(c, x, s, None)
+
+
+process(test.splitlines())
+sigs = [20, 60, 100, 140, 180, 220]
+print(sum([signals[x] for x in sigs]))
+crt2 = "".join(crt)
+for i in [crt2[i : i + 40] for i in range(0, len(crt2), 40)]:
+    print(i)
+crt = []
+process(lines)
+print(sum([signals[x] for x in sigs]))
+crt2 = "".join(crt)
+for i in [crt2[i : i + 40] for i in range(0, len(crt2), 40)]:
+    print(i)
---
+++ day9.py
@@ -0,0 +1,177 @@
+from aocd import lines
+import copy
+import numpy as np
+
+test = """R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2"""
+
+test2 = """R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"""
+
+
+class pos:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def adjacent(self, other):
+        return abs(self.x - other.x) <= 1 and abs(self.y - other.y) <= 1
+
+    def is_diagonal(self):
+        return abs(self.x) + abs(self.y) == 2
+
+    def move(self, other):
+        if self.adjacent(other):
+            return
+        else:
+            (xdiff, ydiff) = (other.x - self.x, other.y - self.y)
+            if abs(xdiff) < 2 and abs(ydiff) == 2:
+                self.x = other.x
+                self.add(pos(0, 1)) if ydiff == 2 else self.add(pos(0, -1))
+            elif abs(ydiff) < 2 and abs(xdiff) == 2:
+                self.y = other.y
+                self.add(pos(1, 0)) if xdiff == 2 else self.add(pos(-1, 0))
+            elif abs(xdiff) == 2 and abs(ydiff) == 2:
+                self.add(pos(xdiff // 2, ydiff // 2))
+
+    def direction(self, other):
+        x = self.x - other.x
+        y = self.y - other.y
+        return pos(x, y)
+
+    def add(self, other):
+        self.x += other.x
+        self.y += other.y
+
+    def set(self, other):
+        self.x = other.x
+        self.y = other.y
+
+    def coord(self):
+        return (self.x, self.y)
+
+    def __repr__(self):
+        return f"({self.x}, {self.y})"
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+
+h_changer = {"R": pos(1, 0), "L": pos(-1, 0), "U": pos(0, 1), "D": pos(0, -1)}
+
+
+def mover(move, knots):
+    prev = copy.deepcopy(knots)
+    knots[0].add(move)
+    for i in range(1, len(knots)):
+        knots[i].move(knots[i - 1])
+        # if not knots[i].adjacent(knots[i - 1]):
+        # direc = knots[i].direction(prev[i - 1])
+        # if direc.is_diagonal():
+        # knots[i].add(direc)
+        # else:
+        # knots[i].set(prev[i - 1])
+
+
+def print_knots(knots, bh):
+    minx, miny, maxx, maxy = gridsize(test2.splitlines())
+    coords = [a.coord() for a in knots]
+
+    # print(minx, miny, maxx, maxy)
+    shift = (-minx, -miny)
+    cols = maxx + 1 - minx
+    rows = maxy + 1 - miny
+    z = [["."] * cols] * rows
+
+    for y in range(maxy, miny - 1, -1):
+        for x in range(minx, maxx):
+            if (x, y) in coords:
+                print(coords.index((x, y)), end="")
+            elif (x, y) == (0, 0):
+                print("s", end="")
+            elif (x, y) in bh:
+                print("#", end="")
+            else:
+                print(".", end="")
+        print()
+    print("===================")
+
+
+def niner(lst: list):
+    prev = None
+    knots = [
+        pos(0, 0),
+        pos(0, 0),
+        pos(0, 0),
+        pos(0, 0),
+        pos(0, 0),
+        pos(0, 0),
+        pos(0, 0),
+        pos(0, 0),
+        pos(0, 0),
+        pos(0, 0),
+    ]
+
+    been_here = [knots[len(knots) - 1].coord()]
+    for l in [l.split() for l in lst]:
+        for i in range(int(l[1])):
+            mover(h_changer[l[0]], knots)
+            been_here.append(knots[len(knots) - 1].coord())
+            # print_knots(knots, been_here)
+    return len(set(been_here))
+
+
+def somer(lst: list):
+    prev = None
+    h_xy = pos(0, 0)
+    t_xy = pos(0, 0)
+    been_here = [t_xy.coord()]
+    move = False
+    for l in [l.split() for l in lst]:
+        prev = copy.deepcopy(h_xy)
+        for i in range(int(l[1])):
+            h_xy.add(h_changer[l[0]])
+            if not h_xy.adjacent(t_xy):
+                been_here.append(prev.coord())
+                t_xy = copy.deepcopy(prev)
+            prev = copy.deepcopy(h_xy)
+    return len(set(been_here))
+
+
+def gridsize(t):
+    maxx, maxy, minx, miny = 0, 0, 0, 0
+    xy = pos(0, 0)
+    for x in t:
+        l = x.split()
+        if l[0] == "R":
+            xy.add(pos(int(l[1]), 0))
+            maxx = max(xy.coord()[0], maxx)
+        if l[0] == "L":
+            xy.add(pos(-int(l[1]), 0))
+            minx = min(xy.coord()[0], minx)
+        if l[0] == "U":
+            xy.add(pos(0, int(l[1])))
+            maxy = max(xy.coord()[1], maxy)
+        if l[0] == "D":
+            xy.add(pos(0, -int(l[1])))
+            miny = min(xy.coord()[1], miny)
+    return minx, miny, maxx, maxy
+
+
+print(somer(test.splitlines()))
+print(somer(lines))
+print(niner(test.splitlines()))
+print(niner(test2.splitlines()))
+print(niner(lines))
